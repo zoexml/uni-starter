c90b55b160e105c48699fad5dbbffec8e4647d64
shiwenjie <1570466620@qq.com>
2025-08-26 09:56:35 +0800

    feat: ✨ tabbar配置

--- pages.config.ts
+++ pages.config.ts
@@ -1,4 +1,5 @@
 import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
+import { tabBar } from './src/tabbar/config'
 
 export default defineUniPages({
   globalStyle: {
@@ -18,27 +19,6 @@ export default defineUniPages({
       '^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
     },
   },
-  tabBar: {
-    color: '#999999',
-    selectedColor: '#018d71',
-    backgroundColor: '#F8F8F8',
-    borderStyle: 'black',
-    fontSize: '10px',
-    iconWidth: '24px',
-    spacing: '3px',
-    list: [
-      {
-        text: '首页',
-        pagePath: 'pages/index/index',
-        iconPath: 'static/tabs/home-default.png',
-        selectedIconPath: 'static/tabs/home-active.png',
-      },
-      {
-        text: '我的',
-        pagePath: 'pages/my/index',
-        iconPath: 'static/tabs/my-default.png',
-        selectedIconPath: 'static/tabs/my-active.png',
-      },
-    ],
-  },
+  // tabbar 的配置统一在 “./src/tabbar/config.ts” 文件中
+  tabBar: tabBar as any,
 })
